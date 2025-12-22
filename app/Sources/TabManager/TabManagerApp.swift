192b4a6063b55ab63cc75deac229a7cbb380577e
muiz <muiz.dev@gmail.com>
2025-12-21 21:58:00 -0800

    impl uds for communication b/w daemon and gui

--- app/Sources/TabManager/TabManagerApp.swift
+++ app/Sources/TabManager/TabManagerApp.swift
@@ -1,14 +1,122 @@
 import SwiftUI
+import Foundation
 
 // NOTE: For ensuring that the application has focus on open.
 // Might not be necessary -- seems the issues I am facing might just be
 // a quark of executing the binary from the terminal.
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private var serverSocket: Int32 = -1
+    private let socketPath = "/tmp/tabmanager.sock"
+
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.regular)
         DispatchQueue.main.async {
             NSRunningApplication.current.activate(options: .activateIgnoringOtherApps)
             NSApp.windows.first?.makeKeyAndOrderFront(nil)
+        }
+        startUDSServer()
+    }
+
+    private func startUDSServer() {
+        unlink(socketPath)
+        serverSocket = socket(AF_UNIX, SOCK_STREAM, 0)
+        guard serverSocket >= 0 else {
+            print("App: Failed to create socket")
+            return
+        }
+
+        var addr = sockaddr_un()
+        addr.sun_family = sa_family_t(AF_UNIX)
+        let pathLen = socketPath.withCString { Int(strlen($0)) }
+        _ = socketPath.withCString { src in
+            withUnsafeMutablePointer(to: &addr.sun_path) { dest in
+                let destPtr = UnsafeMutableRawPointer(dest).assumingMemoryBound(to: Int8.self)
+                strncpy(destPtr, src, 104) // 104 is the max length for sun_path
+            }
+        }
+        addr.sun_len = UInt8(MemoryLayout<sa_family_t>.size + MemoryLayout<UInt8>.size + pathLen + 1)
+
+        let addrLen = socklen_t(addr.sun_len)
+        let bindResult = withUnsafePointer(to: &addr) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                Darwin.bind(serverSocket, $0, addrLen)
+            }
+        }
+
+        guard bindResult == 0 else {
+            print("App: Failed to bind socket. Error: \(String(cString: strerror(errno)))")
+            return
+        }
+
+        guard Darwin.listen(serverSocket, 5) == 0 else {
+            print("App: Failed to listen on socket")
+            return
+        }
+
+        print("App: UDS Server started at \(socketPath)")
+
+        Thread.detachNewThread {
+            self.acceptConnections()
+        }
+    }
+
+    private func acceptConnections() {
+        while true {
+            var clientAddr = sockaddr_un()
+            var len = socklen_t(MemoryLayout<sockaddr_un>.size)
+            let clientSocket = withUnsafeMutablePointer(to: &clientAddr) {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                    Darwin.accept(self.serverSocket, $0, &len)
+                }
+            }
+
+            if clientSocket >= 0 {
+                print("App: Accepted new UDS connection")
+                Thread.detachNewThread {
+                    self.handleClient(clientSocket)
+                }
+            }
+        }
+    }
+
+    private func handleClient(_ clientSocket: Int32) {
+        let bufferSize = 4096
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        defer {
+            buffer.deallocate()
+            close(clientSocket)
+        }
+
+        while true {
+            let bytesRead = read(clientSocket, buffer, bufferSize)
+            if bytesRead > 0 {
+                let data = Data(bytes: buffer, count: bytesRead)
+                if let message = String(data: data, encoding: .utf8) {
+                    print("App: Received UDS message: \(message)")
+                    // Verification log
+                    let logMsg = "[\(Date())] UDS Data: \(message)\n"
+                    try? logMsg.appendLineTo(path: "/tmp/tabmanager_uds.log")
+                }
+            } else if bytesRead == 0 {
+                print("App: UDS connection closed by peer")
+                break
+            } else {
+                print("App: UDS read error")
+                break
+            }
+        }
+    }
+}
+
+extension String {
+    func appendLineTo(path: String) throws {
+        let data = (self + "\n").data(using: .utf8)!
+        if let fileHandle = FileHandle(forWritingAtPath: path) {
+            fileHandle.seekToEndOfFile()
+            fileHandle.write(data)
+            fileHandle.closeFile()
+        } else {
+            try data.write(to: URL(fileURLWithPath: path), options: .atomic)
         }
     }
 }
--- daemon/main.c
+++ daemon/main.c
@@ -1,9 +1,15 @@
+#include <errno.h>
 #include <libwebsockets.h>
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
 
 static int interrupted;
+static int uds_fd = -1;
+static const char *uds_path = "/tmp/tabmanager.sock";
 
 static int callback_http(struct lws *wsi, enum lws_callback_reasons reason,
                          void *user, void *in, size_t len) {
@@ -28,6 +34,16 @@ static int callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
   case LWS_CALLBACK_RECEIVE:
     lwsl_user("LWS_CALLBACK_RECEIVE (%lu bytes): %s\n", (unsigned long)len,
               (const char *)in);
+
+    if (uds_fd >= 0) {
+      if (send(uds_fd, in, len, 0) < 0) {
+        lwsl_err("Daemon: Failed to send data to App via UDS: %s\n",
+                 strerror(errno));
+      } else {
+        lwsl_user("Daemon: Forwarded message to App via UDS\n");
+      }
+    }
+
     // Echo back for testing
     lws_write(wsi, in, len, LWS_WRITE_TEXT);
     break;
@@ -53,12 +69,42 @@ void sigint_handler(int sig) {
   interrupted = 1;
 }
 
+static void init_uds_client(void) {
+  uds_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (uds_fd < 0) {
+    lwsl_err("Daemon: Failed to create UDS socket: %s\n", strerror(errno));
+    return;
+  }
+
+  struct sockaddr_un addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sun_family = AF_UNIX;
+  strncpy(addr.sun_path, uds_path, sizeof(addr.sun_path) - 1);
+
+  if (connect(uds_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    lwsl_err("Daemon: Failed to connect to App UDS at %s: %s\n", uds_path,
+             strerror(errno));
+    close(uds_fd);
+    uds_fd = -1;
+    return;
+  }
+
+  lwsl_user("Daemon: Connected to App UDS at %s\n", uds_path);
+
+  // Send startup ping
+  const char *ping = "{\"event\":\"daemon_startup\",\"data\":\"ping\"}";
+  send(uds_fd, ping, strlen(ping), 0);
+}
+
 int main(void) {
   struct lws_context_creation_info info;
   struct lws_context *ctx;
   int n = 0;
 
   signal(SIGINT, sigint_handler);
+
+  // Initialize UDS Connection to the App
+  init_uds_client();
 
   memset(&info, 0, sizeof info);
   // TODO: Put the websocket port in some configuratble location so that the
@@ -81,6 +127,9 @@ int main(void) {
     n = lws_service(ctx, 0);
   }
 
+  if (uds_fd >= 0) {
+    close(uds_fd);
+  }
   lws_context_destroy(ctx);
   return 0;
 }
