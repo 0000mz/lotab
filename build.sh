f1f4a95116f308438c6cc9e368148bfa963fb029
muiz <muiz.dev@gmail.com>
2025-12-27 01:19:57 -0800

    allow specifying custom meson path for build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MESON=${MESON:-meson}
+
 BUILD_DIR="build"
 BUILD_TYPE="debug"
 
@@ -9,11 +11,11 @@ fi
 
 if [ ! -d "$BUILD_DIR" ]; then
     echo "Setting up build directory ($BUILD_TYPE)..."
-    meson setup "$BUILD_DIR" --buildtype="$BUILD_TYPE"
+    "$MESON" setup "$BUILD_DIR" --buildtype="$BUILD_TYPE"
 else
     echo "Configuring build type to $BUILD_TYPE..."
-    meson configure "$BUILD_DIR" -Dbuildtype="$BUILD_TYPE"
+    "$MESON" configure "$BUILD_DIR" -Dbuildtype="$BUILD_TYPE"
 fi
 
 echo "Compiling daemon..."
-meson compile -C "$BUILD_DIR"
+"$MESON" compile -C "$BUILD_DIR"
